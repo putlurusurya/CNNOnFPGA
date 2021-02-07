f9a5f4c54596530109625e2602d1245f7e8154ce
putlurusurya <43723182+putlurusurya@users.noreply.github.com>
2021-02-07 19:52:10 +0530

    update

--- sources_1/new/demux_array.v
+++ sources_1/new/demux_array.v
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
@@ -48,30 +47,3 @@ module demux_array#(
       end
   endgenerate
   endmodule
-=======
-// Code your design here
-module demux_array#(
-  parameter array_size = 9,
-  parameter data_size = 8
-)(input [data_size*array_size:0]d_in,
-  output [data_size*array_size:0]d_out_1,
-  output [data_size*array_size:0]d_out_2,
-  output [data_size*array_size:0]d_out_3,
-  output [data_size*array_size:0]d_out_4);
-  
-  
-  genvar i;
-  generate
-    for(i=0;i<array_size;i++)
-      begin
-      demux1_4 uut(d_in[(i+1)*datasize-1:0],
-                   sel[1:0],
-                   d_out_1[(i+1)*datasize-1:0],
-                   d_out_2[(i+1)*datasize-1:0],
-                   d_out_3[(i+1)*datasize-1:0],
-                   d_out_4[(i+1)*datasize-1:0]);
-      end
-  endgenerate
-  endmodule
-  
->>>>>>> 2f34efcdbe260cedbea9d23709aa717beb1863d5
